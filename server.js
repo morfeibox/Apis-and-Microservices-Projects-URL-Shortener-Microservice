af7e1b08825200947954445e26a3ddecc652c61f
Atanas Beychev <abeychev@dd-fs.co.uk>
2019-10-15 16:29:56 +0300

    create form and get action

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser')
+require('dotenv').config({path: __dirname + '/.env'})
 
 var cors = require('cors');
 
@@ -18,6 +20,8 @@ app.use(cors());
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -31,7 +35,15 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+// POST /login gets urlencoded bodies
+app.post('/api/shorturl/new/', urlencodedParser, function (req, res) {
+ 
+  res.json({"original_url": req.body.url_to_shrten, "short_url":1});
+ })
 
-app.listen(port, function () {
-  console.log('Node.js listening ...');
-});
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+})
